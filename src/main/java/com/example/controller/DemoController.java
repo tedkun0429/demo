a256fcde599b7c9d57d974739a2bcef0ff69a083
tukun.sc <tedkun1223@126.com>
2019-04-14 16:56:43 +0800

    requestmapping 配置url映射 controller     配置http请求 restcontroller 配置ajax请求

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,13 +1,27 @@
 package com.example.controller;
 
+import com.example.properties.MysqlProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class DemoController {
+    @Resource
+    private MysqlProperties mysqlProperties;
 
     @RequestMapping("/hello")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @RequestMapping("/showProperties")
+    public String showProperties() {
+        return "mysql.jdbcName:" + mysqlProperties.getJdbcName() + "<br>"
+                + "mysql.dbUrl:" + mysqlProperties.getDbUrl() + "<br>"
+                + "mysql.userName:" + mysqlProperties.getUserName() + "<br>"
+                + "mysql.password:" + mysqlProperties.getPassword() + "<br>"
+                + "mysql.testStr:" + mysqlProperties.getTestStr() + "<br>";
     }
 }
