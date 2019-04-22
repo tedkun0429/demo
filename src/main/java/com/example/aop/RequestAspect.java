68e78e4179f01e15508e503dfbcb3831fde3a923
tedkun1223@126.com <tedkun1223@126.com>
2019-04-22 16:14:01 +0800

    aop编程

--- src/main/java/com/example/aop/RequestAspect.java
+++ src/main/java/com/example/aop/RequestAspect.java
@@ -1,4 +1,49 @@
 package com.example.aop;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
 public class RequestAspect {
+
+    private Logger logger = Logger.getLogger(RequestAspect.class);
+
+    /**
+     * 对com.example.controller包下的任意类的任意方法任意参数的public方法
+     */
+    @Pointcut("execution(public * com.example.controller.*.*(..))")
+    public void log(){
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        logger.info("方法执行前...");
+        ServletRequestAttributes sra = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = sra.getRequest();
+        logger.info("url:" + request.getRequestURI());
+        logger.info("ip:" + request.getRemoteHost());
+        logger.info("method:" + request.getMethod());
+        logger.info("class_method:" + joinPoint.getSignature().getDeclaringTypeName()+"."
+                + joinPoint.getSignature().getName());
+        logger.info("args:" + joinPoint.getArgs());
+    }
+
+    @After("log()")
+    public void doAfter(JoinPoint joinPoint) {
+        logger.info("方法执行后...");
+    }
+
+    @AfterReturning(returning = "result", pointcut = "log()")
+    public void doAfterReturning(Object result) {
+        logger.info("方法返回值:" + result);
+    }
 }
+
